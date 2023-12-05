50a8b1f08576d585e43f368f64db616395b14813
Nelisa Dludla <nelisa.dludla@outlook.com>
2023-12-05 20:31:11 +0200

    Updated BaseModel with task 4 requirements

--- models/base_model.py
+++ models/base_model.py
@@ -8,10 +8,23 @@ class BaseModel():
     created_at = datetime.now()
     updated_at = created_at
 
-    def __init__(self):
-        self.id = BaseModel.id
-        self.created_at = BaseModel.created_at.now()
-        self.updated_at = BaseModel.updated_at
+    def __init__(self, *args, **kwargs):
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    format = "%Y-%m-%d %H:%M:%S.%f"
+                    new_value = datetime.strptime(value, format)
+
+                    setattr(self, key, new_value)
+
+                elif key != "__class__":
+                    setattr(self, key, value)
+
+        else:
+            self.id = BaseModel.id
+            self.created_at = BaseModel.created_at.now()
+            self.updated_at = BaseModel.updated_at
 
     def save(self):
         self.updated_at = datetime.now()
@@ -21,6 +34,7 @@ class BaseModel():
         for key, value in self.__dict__.items():
             result_dict[f"{key}"] = f"{value}"
 
+        result_dict[f"__class__"] = self.__class__.__name__
         return result_dict
 
     def __str__(self):
