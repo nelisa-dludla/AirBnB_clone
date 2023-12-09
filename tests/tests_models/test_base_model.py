2d6e86a75605dbadd81270039f347c372a9934d3
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-12-09 14:43:07 +0200

    Test_base_model update

--- tests/tests_models/test_base_model.py
+++ tests/tests_models/test_base_model.py
@@ -11,6 +11,7 @@ from datetime import datetime
 from time import sleep
 from models.base_model import BaseModel
 
+
 class TestBaseModel_instantiation(unittest.TestCase):
     """Unittests for testing instantiation of the BaseModel class."""
 
@@ -26,13 +27,12 @@ class TestBaseModel_instantiation(unittest.TestCase):
     def test_updated_at_is_public_datetime(self):
         self.assertEqual(datetime, type(BaseModel().updated_at))
 
-
     def test_two_models_different_created_at(self):
         bm1 = BaseModel()
         sleep(0.05)
         bm2 = BaseModel()
         self.assertLess(bm1.created_at, bm2.created_at)
-    
+
     def test_args_unused(self):
         bm = BaseModel(None)
         self.assertNotIn(None, bm.__dict__.values())
@@ -58,14 +58,12 @@ class TestBaseModel_instantiation(unittest.TestCase):
         self.assertEqual(bm.updated_at, dt)
 
 
-
 class TestBaseModel_to_dict(unittest.TestCase):
     """Unittests for testing to_dict method of the BaseModel class."""
 
     def test_to_dict_type(self):
         bm = BaseModel()
         self.assertTrue(dict, type(bm.to_dict()))
-
 
     def test_to_dict_contains_added_attributes(self):
         bm = BaseModel()
@@ -79,7 +77,7 @@ class TestBaseModel_to_dict(unittest.TestCase):
         bm_dict = bm.to_dict()
         self.assertEqual(str, type(bm_dict["created_at"]))
         self.assertEqual(str, type(bm_dict["updated_at"]))
-    
+
     def test_to_dict_output(self):
         dt = datetime.today()
         bm = BaseModel()
@@ -92,9 +90,9 @@ class TestBaseModel_to_dict(unittest.TestCase):
         }
         self.assertDictEqual(bm.to_dict(), tdict)
 
+
 class TestBaseModel_save(unittest.TestCase):
     """Unittests for testing save method of the BaseModel class."""
-
     @classmethod
     def setUp(self):
         try:
@@ -142,7 +140,6 @@ class TestBaseModel_save(unittest.TestCase):
         bmid = "BaseModel." + bm.id
         with open("file.json", "r") as f:
             self.assertIn(bmid, f.read())
-    
 
 
 if __name__ == "__main__":
